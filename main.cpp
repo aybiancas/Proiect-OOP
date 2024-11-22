b4084986f7f4e2842dabfab5a3fa8718a4806086
Bianca Sandu <biancasandu998@gmail.com>
2024-11-22 22:34:16 +0200

    Code fixes.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,15 @@
-#include <iostream>
-#include <random>
-#include <fstream>
 #include "headers/menu.h"
-
-
-//test reupstream
 
 int main() {
 
     // menu test
-
-    std::ifstream f("tastatura.txt");
-    int choice;
-    f >> choice;
-    f.close();
+    //
+    // std::ifstream f("tastatura.txt");
+    // int choice;
+    // f >> choice;
+    // f.close();
 
     Menu menu;
-    menu.run(choice);
+    menu.run();
 
 }
