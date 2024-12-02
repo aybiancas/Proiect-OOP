83a6d3071ff81c1bd4569b7b35600d2d895c4f0b
Bianca Sandu <biancasandu998@gmail.com>
2024-12-02 19:14:53 +0200

    Code updates for derived class

--- sources/dealerPlayer.cpp
+++ sources/dealerPlayer.cpp
@@ -1,26 +1,20 @@
 #include "../headers/dealerPlayer.h"
 
-DealerPlayer::DealerPlayer() :
-    Player(),
-    pot(0) {
+    DealerPlayer::DealerPlayer() :
+        Player(),
+        pot(0) {}
 
-}
+    DealerPlayer::DealerPlayer(const DealerPlayer &other) :
+        Player(other),
+        pot(other.pot) {}
 
-DealerPlayer::DealerPlayer(const DealerPlayer &other) :
-    Player(other),
-    pot(other.pot) {
+    DealerPlayer::~DealerPlayer() {}
 
-}
+    int DealerPlayer::getPot() const {
+        return pot;
+    }
 
-DealerPlayer::~DealerPlayer() {
-
-}
-
-int DealerPlayer::getPot() const {
-    return pot;
-}
-
-void DealerPlayer::addPot(int amount) {
-    pot += amount;
-}
+    void DealerPlayer::addPot(int amount) {
+        pot += amount;
+    }
 
