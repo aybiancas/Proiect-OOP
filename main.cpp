bee129aa0461a51917df4fbcf784978871097763
Bianca Sandu <biancasandu998@gmail.com>
2024-12-02 13:13:35 +0200

    Added exception test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include "headers/menu.h"
+#include "headers/BetInputInvalidExcept.h"
+#include "headers/FileLoadFailureExcept.h"
+#include "headers/NotEnoughFundsExcept.h"
 
 int main() {
 
@@ -9,7 +12,15 @@ int main() {
     // f >> choice;
     // f.close();
 
-    Menu menu;
-    menu.run();
+    try {
+        Menu menu;
+        menu.run();
+    } catch (FileLoadFailure& e) {
+        std::cout << "FileLoadFailure: " << e.what() << std::endl;
+    } catch (NotEnoughFundsExcept& e) {
+        std::cout << "NotEnoughFundsExcept: " << e.what() << std::endl;
+    } catch (BetInputInvalidExcept& e) {
+        std::cout << "BetInputInvalidExcept: " << e.what() << std::endl;
+    }
 
 }
