ec683caa666627926c532a483da9428a4d91c761
Bianca Sandu <biancasandu998@gmail.com>
2025-01-03 15:44:04 +0200

    Code fixes

--- sources/card.cpp
+++ sources/card.cpp
@@ -58,13 +58,3 @@ const sf::Texture &Card::getTexture() const {
 	return texture;
 }
 
-// void Card::setSprite(const sf::Sprite &_sprite) {
-// 	this->sprite = _sprite;
-// }
-
-// void Card::loadTexture(const std::string &path) {
-// 	if (!texture.loadFromFile(path)) {
-// 		throw FileLoadFailure("Error: Failed to load card texture");
-// 	}
-// 	sprite.setTexture(texture);
-// }
